131176d902b09148ce3a3cb9db9056cbde718366
aanciaes <aanciaes.miguel@gmail.com>
2019-04-27 02:09:56 +0100

    Http logger now logs the status code of the response

--- handlers/LoggingWrapper.go
+++ handlers/LoggingWrapper.go
@@ -4,10 +4,25 @@ import (
 	"io"
 	"net"
 	"net/http"
+	"strconv"
 	"time"
 )
 
-func writeLogLine (out io.Writer, req *http.Request) {
+type LogRecorder struct {
+	http.ResponseWriter
+	status int
+}
+
+func (r *LogRecorder) Write(p []byte) (int, error) {
+	return r.ResponseWriter.Write(p)
+}
+
+func (rec *LogRecorder) WriteHeader(code int) {
+	rec.status = code
+	rec.ResponseWriter.WriteHeader(code)
+}
+
+func writeLogLine (out io.Writer, req *http.Request, status int) {
 	host, _, err := net.SplitHostPort(req.RemoteAddr)
 
 	if err != nil {
@@ -22,12 +37,15 @@ func writeLogLine (out io.Writer, req *http.Request) {
 	buf = append(buf, " - "...)
 	buf = append(buf, `[ "`...)
 	buf = append(buf, t.Format("02/Jan/2006:15:04:05 -0700")...)
-	buf = append(buf, `]`...)
+	buf = append(buf, `] `...)
 	buf = append(buf, req.Method...)
 	buf = append(buf, " "...)
 	buf = append(buf, uri...)
 	buf = append(buf, " "...)
 	buf = append(buf, req.Proto...)
+	buf = append(buf, `" `...)
+	buf = append(buf, strconv.Itoa(status)...)
+	buf = append(buf, " "...)
 	buf = append(buf, ` "`...)
 	buf = append(buf, req.Referer()...)
 	buf = append(buf, `"`...)
@@ -36,12 +54,14 @@ func writeLogLine (out io.Writer, req *http.Request) {
 	out.Write(buf)
 }
 
-
 func LoggingWrapper (out io.Writer, h http.Handler) http.Handler {
 
 
 	return http.HandlerFunc( func (w http.ResponseWriter, r *http.Request) {
-		writeLogLine(out, r)
-		h.ServeHTTP(w, r)
+		rec := LogRecorder{w, 200}
+
+		h.ServeHTTP(&rec, r)
+
+		writeLogLine(out, r, rec.status)
 	})
 }
