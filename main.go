a152c6401009076a6849936bca298d3d05ca2476
aanciaes <aanciaes.miguel@gmail.com>
2019-04-30 13:33:12 +0100

    Added ssl encryption

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
+	"crypto/tls"
 	"github.com/aanciaes/smart-grow-api/config/database"
 	"github.com/aanciaes/smart-grow-api/handlers"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
-	"os"
 )
 
 func setHandlers (r *mux.Router) {
@@ -22,8 +22,36 @@ func main() {
 	setHandlers(r)
 
 	if err == nil {
-		log.Println("Starting server, listening at port 8000")
-		log.Fatal(http.ListenAndServe(":8000", handlers.LoggingWrapper(os.Stdout, r)))
+		cfg := &tls.Config{
+			MinVersion:               tls.VersionTLS11,
+			CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+			PreferServerCipherSuites: true,
+			CipherSuites: []uint16{
+				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+				tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256,
+				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+				tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256,
+			},
+		}
+
+		srv := &http.Server{
+			Addr:         ":443",
+			Handler:      r,
+			TLSConfig:    cfg,
+			TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler), 0),
+		}
+
+		log.Println("Starting SSL server, listening at port 8443")
+		log.Fatal(srv.ListenAndServeTLS("keys/tls.crt", "keys/tls.key"))
 	} else {
 		log.Fatal(err)
 	}
